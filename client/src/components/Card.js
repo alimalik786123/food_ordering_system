07d181e764f7edf917850591e99c32b721344d21
alimalik786123 <alimalik123010@gmail.com>
2023-03-14 23:09:48 +0530

    commit

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Card=(props)=>{
    return( <div class="card m-2" style={{"width": "15rem"}}>
-   <img src={props.image} class="card-img-top h-100 w-300" alt="..."/>
+   <img src={props.image} class="card-img-top " style={{height:"200px",objectFit:"fill"}} alt="..."/>
    <div class="card-body">
      <p class="card-text">{props.foodname} </p>
      <div className="container w-100"><select className="m-2  h-100 bg-gradient rounded-3" name="list" id="">{Array.from(Array(6),(e,i)=>{
@@ -9,7 +9,11 @@ const Card=(props)=>{
      })}</select>
      <select name="" className="m-2  h-100 bg-gradient rounded-3" id=""><option value="half">half</option><option value="half">half</option></select>
      <div className="d-inline fs-5">Total Price : {props.price}</div>
+    
      </div>
+     <hr />
+     <button className="btn btn-success justify-center ms-2"> <i class="bi bi-cart-plus"></i>
+add cart</button>
    </div>
    
  </div>)
---
+++ client/src/components/Contextreducer.js
@@ -0,0 +1,8 @@
+import React, { createContext, useReducer } from "react";
+const Cartcontext=createContext();
+const cartdispatch=createContext();
+const Cartprovider=()=>{
+    const[state,dispatch]=useReducer()
+    
+}
+export default Cartprovider
